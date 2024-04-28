8964c1834090f59af9365a50ea382a1cd8879b02
Hussein7915 <41763711+Hussein7915@users.noreply.github.com>
2024-04-28 14:53:34 +0000

    a

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
-def homeViewset(request):
-    return HttpResponse("Hello Wrold")
+def home(request):
+    return HttpResponse('Hello Wrold')
